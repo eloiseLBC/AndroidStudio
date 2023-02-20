e67df7a9ed949b0e78efc324fd6a2d54db6fed47
eloiseLBC <eloise.leblanc@hotmail.com>
2023-02-20 11:33:21 +0100

    Logo2secondes + Page Home

--- app/src/main/java/fr/projectone/salade/HomeActivity.kt
+++ app/src/main/java/fr/projectone/salade/HomeActivity.kt
@@ -3,9 +3,9 @@ package fr.projectone.salade
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
     }
 }
